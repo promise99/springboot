7e40cc31c3cc6fcffac32a158cca1c8632b34548
acmen <1076963792@qq.com>
2017-12-19 23:34:30 +0800

    呵呵
    添加get和set方法

--- src/main/java/com/entity/User.java
+++ src/main/java/com/entity/User.java
@@ -9,12 +9,27 @@ public class User {
     private Integer age ;
     private String name;
 
+    public Integer getAge() {
+        return age;
+    }
 
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
-
-
-
+    public void setName(String name) {
+        this.name = name;
+    }
 }
